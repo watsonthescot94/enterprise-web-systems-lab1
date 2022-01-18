556f80d34d136a2b6c00cb353d30c4ed4cc8c416
STEPHEN WATSON (2006308) <s.watson19@rgu.ac.uk>
2022-01-18 16:30:17 +0000

    Adding html_one

--- main_html.js
+++ main_html.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) => {
  res.writeHead(200, { 'content-type': 'text/html' })
- fs.createReadStream('myWebPage.html').pipe(res)
+ fs.createReadStream('html_one.html').pipe(res)
 })
 server.listen(process.env.PORT || 3000)
 console.log('Server running at http://127.0.0.1:3000/');
